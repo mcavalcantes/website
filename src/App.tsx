d3e87148f2e4e4e93d73a3f2c13c515b20bd5b1d
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2024-10-12 12:02:00 -0300

    modified the global styling of heading elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,11 @@
 export const App = () => {
+  return (
+    <section>
+      <h1>This is a h1</h1>
+      <h2>This is a h2</h2>
+      <h3>This is a h3</h3>
+      <p>This is a p</p>
+      <p>This is a small p</p>
+    </section>
+  );
 }
