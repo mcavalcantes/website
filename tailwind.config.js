4f1ee3e3335108012103d7d765d281e4c33ca9be
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2024-10-12 23:02:51 -0300

    added the color palette to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light": {
+          "text": "#262521",
+          "border": "#C6AF96",
+          "hover": "#F4E0CD",
+          "active": "#E3CAB2",
+          "background": "#FFF4E6",
+        },
+        "dark": {
+          "text": "#FFF4E6",
+          "border": "#776856",
+          "hover": "#3E382E",
+          "active": "#574D40",
+          "background": "#262521",
+        },
+      },
+    },
   },
   plugins: [],
 }
